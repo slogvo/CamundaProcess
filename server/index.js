c401082c30b4cee0d1caef3d5578a4503e0e5ab6
longvoct <86196042+longvoct@users.noreply.github.com>
2022-06-01 13:22:26 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -59,33 +59,6 @@ app.listen(5000, () => {
 // client.subscribe("test_demo", async function ({ task, taskService }) {
 //   // Put your business logic
 //   // complete the task
-//   // app.use(
-//   //   cors({
-//   //     origin: "*",
-//   //     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   //     preflightContinue: false,
-//   //     optionsSuccessStatus: 204,
-//   //   })
-//   // );
-//   // dotenv.config();
-//   // app.use(express.json());
-//   // //ROUTES
-//   // app.use("/v1/category", categoryRoute);
-//   // mongoose
-//   //   .connect(process.env.MONGO_URL, {
-//   //     useNewUrlParser: true,
-//   //     useUnifiedTopology: true,
-//   //   })
-//   //   .then(console.log("Connected to MongoDB"))
-//   //   .catch((err) => console.error(err));
-//   // app.use(bodyParser.json({ limit: "50mb" }));
-//   // app.use(cors());
-//   // app.use(morgan("common"));
-//   // app.get("/", (req, res) => {
-//   //   res.send("Hello world!");
-//   // });
-//   // app.listen(5000, () => {
-//   //   console.log("Backend is running on port 5000");
-//   // });
+
 //   // await taskService.complete(task);
 // });
